b315f77801c9113b89036fee87c7173cffa4d53b
Liam Wright <lwright02@LiamThinkpad.>
2025-05-23 19:03:13 -0500

    Incorporated art into GUI and fixed the frame

--- src/gui.py
+++ src/gui.py
@@ -5,7 +5,8 @@ GUI for Strands
 import click, random, os, pygame, sys, math
 from strands import Pos, Strand, StrandsGame
 from base import Step, PosBase, StrandBase, BoardBase, StrandsGameBase
-from ui import ArtGUIStub
+from ui import ArtGUIStub, ArtGUIBase
+from art_gui import ArtGUI9Slice, ArtGUICat3, ArtGUICat4
 
 COLORS: dict[str, tuple[int, int, int]] = {
     "WHITE": (255, 255, 255), 
@@ -17,30 +18,30 @@ COLORS: dict[str, tuple[int, int, int]] = {
     }
 CELL_SIZE: int = 50
 
-
 def refresh_board(surface: pygame.surface.Surface, strands: StrandsGameBase, 
-    pending: list[Pos]) -> None:
+    pending: list[Pos], art: ArtGUIBase) -> None:
     """
     Draws the current state of the Board
     """
-    #Uses the ArtGUIStub to draw the interior and frame
-    art = ArtGUIStub(frame_width = 15)
+    #Uses the imported art to dra the frame
     art.draw_background(surface)
-    border = art.frame_width
-    inner_rect = pygame.Rect(
-        border,
-        border,
-        surface.get_width() - 2 * border,
-        surface.get_height() - 2 * border
-    )
-    pygame.draw.rect(surface, COLORS["WHITE"], inner_rect)
+    FRAME = art.frame_width + 15
     
     board: BoardBase = strands.board()
+
     rows: int = board.num_rows()
     cols: int = board.num_cols()
-    surface_width: int = CELL_SIZE * cols
-    surface_height: int = CELL_SIZE * (rows + 1)
+    surface_width: int = CELL_SIZE * cols + (2 *FRAME)
+    surface_height: int = CELL_SIZE * (rows + 1) + (2 * FRAME)
     font: pygame.font.Font = pygame.font.SysFont(None, 36)
+    if not isinstance(art, ArtGUICat4):
+        interior_rect = pygame.Rect(
+            FRAME,
+            FRAME,
+            CELL_SIZE * cols,
+            CELL_SIZE * rows
+        )
+        pygame.draw.rect(surface, COLORS["WHITE"], interior_rect)
 
     # Creates a set of positions for the found words and adds the lines between
     # the positions
@@ -51,10 +52,10 @@ def refresh_board(surface: pygame.surface.Surface, strands: StrandsGameBase,
             for i in range(len(positions) - 1):
                 pos_1: PosBase = positions[i]
                 pos_2: PosBase = positions[i + 1]
-                x1: int = pos_1.c * CELL_SIZE + CELL_SIZE // 2
-                y1: int = pos_1.r * CELL_SIZE + CELL_SIZE // 2
-                x2: int = pos_2.c * CELL_SIZE + CELL_SIZE // 2
-                y2: int = pos_2.r * CELL_SIZE + CELL_SIZE // 2
+                x1: int = FRAME + pos_1.c * CELL_SIZE + CELL_SIZE // 2
+                y1: int = FRAME + pos_1.r * CELL_SIZE + CELL_SIZE // 2
+                x2: int = FRAME + pos_2.c * CELL_SIZE + CELL_SIZE // 2
+                y2: int = FRAME + pos_2.r * CELL_SIZE + CELL_SIZE // 2
                 pygame.draw.line(surface, COLORS["LIGHTBLUE"], (x1, y1), 
                     (x2, y2), width=4)
         for pos in positions:
@@ -62,8 +63,8 @@ def refresh_board(surface: pygame.surface.Surface, strands: StrandsGameBase,
 
     # Displays the pending selection with the color green
     for pos in pending:
-        center: tuple[int, int] = (pos.c * CELL_SIZE + CELL_SIZE // 2, 
-            pos.r * CELL_SIZE + CELL_SIZE // 2)
+        center: tuple[int, int] = (FRAME + pos.c * CELL_SIZE + CELL_SIZE // 2, 
+            FRAME + pos.r * CELL_SIZE + CELL_SIZE // 2)
         radius: int = CELL_SIZE // 3
         pygame.draw.circle(surface, COLORS["GREEN"], center, radius)
 
@@ -71,14 +72,14 @@ def refresh_board(surface: pygame.surface.Surface, strands: StrandsGameBase,
         for i in range(len(pending) - 1):
             pending_pos_1: PosBase = pending[i]
             pending_pos_2: PosBase = pending[i + 1]
-            x1 = pending_pos_1.c * CELL_SIZE + CELL_SIZE // 2
-            y1 = pending_pos_1.r * CELL_SIZE + CELL_SIZE // 2
-            x2 = pending_pos_2.c * CELL_SIZE + CELL_SIZE // 2
-            y2 = pending_pos_2.r * CELL_SIZE + CELL_SIZE // 2
+            x1 = FRAME + pending_pos_1.c * CELL_SIZE + CELL_SIZE // 2
+            y1 = FRAME + pending_pos_1.r * CELL_SIZE + CELL_SIZE // 2
+            x2 = FRAME + pending_pos_2.c * CELL_SIZE + CELL_SIZE // 2
+            y2 = FRAME + pending_pos_2.r * CELL_SIZE + CELL_SIZE // 2
             pygame.draw.line(surface, COLORS["GREEN"], (x1, y1), 
                 (x2, y2), width=4)
 
-    # Draws the hints on the board
+    # Draws the hints on the board only if the hint meter allows
     active_hint: tuple[int, bool] | None = strands.active_hint()
     if active_hint is not None:
         index: int
@@ -89,7 +90,7 @@ def refresh_board(surface: pygame.surface.Surface, strands: StrandsGameBase,
         hint_positions: list[PosBase] = hint_strand.positions()
 
         for i, pos in enumerate(hint_positions):
-            center = (pos.c * CELL_SIZE + CELL_SIZE // 2, pos.r * CELL_SIZE + 
+            center = (FRAME + pos.c * CELL_SIZE + CELL_SIZE // 2, FRAME + pos.r * CELL_SIZE + 
                 CELL_SIZE // 2)
             radius = CELL_SIZE // 4
             if ends and (i == 0 or i == len(hint_positions) - 1):
@@ -106,16 +107,16 @@ def refresh_board(surface: pygame.surface.Surface, strands: StrandsGameBase,
             letter: str = board.get_letter(position)
 
             if (row, col) in highlighted_positions:
-                center = (col * CELL_SIZE + CELL_SIZE // 2, 
-                    row * CELL_SIZE + CELL_SIZE // 2)
+                center = (FRAME + col * CELL_SIZE + CELL_SIZE // 2, 
+                    FRAME + row * CELL_SIZE + CELL_SIZE // 2)
                 radius = CELL_SIZE // 3
                 pygame.draw.circle(surface, COLORS["LIGHTBLUE"], center, radius)
 
             letter_surface: pygame.Surface = font.render(letter, True, 
                 COLORS["BLACK"])
             letter_rect: pygame.Rect = letter_surface.get_rect(center = (
-                col * CELL_SIZE + CELL_SIZE // 2, 
-                row * CELL_SIZE + CELL_SIZE // 2)
+                FRAME + col * CELL_SIZE + CELL_SIZE // 2, 
+                FRAME + row * CELL_SIZE + CELL_SIZE // 2)
                 )
             surface.blit(letter_surface, letter_rect)
 
@@ -125,12 +126,13 @@ def refresh_board(surface: pygame.surface.Surface, strands: StrandsGameBase,
         COLORS["BLACK"])
     hint_rect: pygame.Rect = hint_surface.get_rect(center = (
         surface_width //2, 
-        surface_height - (0.5 * CELL_SIZE))
+        surface_height - (0.5 * CELL_SIZE) - FRAME)
         )
+    pygame.draw.rect(surface, COLORS["WHITE"], hint_rect.inflate(20, 10))
     surface.blit(hint_surface, hint_rect)
 
 
-def run_game(filename: str, show: bool = False, hint_threshold: int = 3) -> None:
+def run_game(filename: str, show: bool = False, hint_threshold: int = 3, art: ArtGUIBase | None = None) -> None:
     """
     Plays a game of Strands on a pygame window
     """
@@ -146,8 +148,9 @@ def run_game(filename: str, show: bool = False, hint_threshold: int = 3) -> None
     board: BoardBase = game.board()
     rows: int = board.num_rows()
     cols: int = board.num_cols()
-    surface_width: int = CELL_SIZE * cols
-    surface_height: int = CELL_SIZE * (rows + 1)
+    FRAME = art.frame_width + 15
+    surface_width: int = CELL_SIZE * cols + (2 * FRAME)
+    surface_height: int = CELL_SIZE * (rows + 1) + (2 * FRAME)
     surface: pygame.Surface = pygame.display.set_mode((surface_width, 
         surface_height))
     clock: pygame.time.Clock = pygame.time.Clock()
@@ -178,7 +181,10 @@ def run_game(filename: str, show: bool = False, hint_threshold: int = 3) -> None
                         currently_selected.clear()
 
                     if event.key == pygame.K_h:
-                        game.use_hint()
+                        if game.hint_meter() >= game.hint_threshold():
+                            game.use_hint()
+                        else:
+                            print("Hint meter too low.")
 
             elif event.type == pygame.MOUSEBUTTONDOWN and not show:
                 x: int
@@ -237,7 +243,7 @@ def run_game(filename: str, show: bool = False, hint_threshold: int = 3) -> None
                                     index = currently_selected.index(cell_pos)
                                     currently_selected = currently_selected[:index + 1]
 
-        refresh_board(surface, game, currently_selected)
+        refresh_board(surface, game, currently_selected, art)
         pygame.display.update()
 
         if not show and game.game_over():
@@ -267,11 +273,20 @@ def main(show: bool, game: str | None, hint: int, art: str):
             sys.exit(1)
         filename = f"boards/{random.choice(txt_files)}"
     
-    if art != "stub":
-        print(f"Art frame '{art}' not supported in GUI yet. Only 'stub' is implemented.")
+    if art == "stub":
+        art_frame = ArtGUIStub(frame_width=15)
+    elif art == "9slices":
+        art_frame = ArtGUI9Slice(frame_width=15)
+    elif art == "cat3":
+        art_frame = ArtGUICat3(frame_width=15)
+    elif art == "cat4":
+        art_frame = ArtGUICat4(frame_width=20)
+    else:
+        print(f"Art frame '{art}' not supported in GUI.")
         sys.exit(1)
 
-    run_game(filename, show=show, hint_threshold=hint)
+
+    run_game(filename, show=show, hint_threshold=hint, art = art_frame)
 
 
 if __name__ == "__main__":
